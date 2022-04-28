4362c2001bc20ebdf49805e5c49cb81470737aa3
Exunys <76539058+Exunys@users.noreply.github.com>
2022-04-28 16:13:32 +0200

    Text to ASCII Converter

--- Module.lua
+++ Module.lua
@@ -1,5 +1,5 @@
 return function(String, Type)
-    local Characters, Bytes = {}, {}
+    local Characters, Bytes, Output = {}, {}, ""
 
     for i = 1, string.len(String) do
         table.insert(Characters, string.sub(String, i, i))
@@ -7,17 +7,14 @@ return function(String, Type)
 
     for i, v in next, Characters do
         table.insert(Bytes, string.byte(v, 1, string.len(v)))
-        print("["..i.."]: "..v.." > "..string.byte(v, 1, string.len(v)))
     end
 
-    --// Types
-
-    local Output = ""
-
     if Type == 1 then
-        Output = "local Result = string.char("..tostring(table.concat(Bytes, ", "))..")"
+        Output = "string.char("..tostring(table.concat(Bytes, ", "))..")"
     elseif Type == 2 then
-        Output = "local Result = \"\\"..tostring(table.concat(Bytes, "\\")).."\""
+        Output = "\"\\"..tostring(table.concat(Bytes, "\\")).."\""
+    else
+        error("(Text To ASCII Converter Module) Invalid Type <uint> parsed \""..tostring(Type).."\"", 2)
     end
 
     return Output
